f90912a103ef0b6137018b3b07561d21d40dfae2
aqtagon <ahmed.danawi@gmail.com>
2023-11-25 20:20:20 -0800

    Added to generatemarkdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,29 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+
+function generateMarkdown(data) {
+  return `
+  # ${data.title}
+
+## Description
+${data.description || 'No description provided.'}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Instalation
+${data.installation || 'No installation instructions provided.'}
+
+## Usage
+${data.usage || 'No usage information provided.'}
+
+  `;
+}
+
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
 

cbb4374c390f124ee1f6d5840f88bf0d54e789fb
aqtagon <ahmed.danawi@gmail.com>
2023-11-25 19:59:27 -0800

    Added questions array and console.log

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    console.log(data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {}
 
 // Function call to initialize app
 init();
